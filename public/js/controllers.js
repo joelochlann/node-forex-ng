3c5cbaf52b9d3e119bd696b2ca2710f2d35c59a3
Joseph Smith <joseph.smith@worldfirst.com>
2015-06-29 16:29:12 +0100

    Initial commit. A lot of boilerplate and broken stuff still

---
+++ public/js/controllers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('AppCtrl', function ($scope, socket) {
+    $scope.traders = [
+      new SimpleMovingAverageTrader("SimpleMovingAverageTrader", 50000),
+      new RandomTrader("RandomTrader", 10000),
+      new MovingAverageTrader("MovingAverageTrader", 50000, 10),
+      new RSITrader("RSITrader", 50000, 14)
+    ];
+
+    socket.on('tick', function (tick) {
+      console.log(tick);
+    });
+  }).
+  controller('MyCtrl1', function ($scope, socket) {
+    socket.on('tick', function (tick) {
+      $scope.price = tick.close;
+    });
+  }).
+  controller('MyCtrl2', function ($scope) {
+    // write Ctrl here
+  });
---
+++ public/js/traders/sma-trader.js
@@ -0,0 +1,50 @@
+window.SimpleMovingAverageTrader = (function(){
+    return Trader.extend({
+        buy:false,
+        sell:false,
+        shortPeriod: 5,
+        longPeriod: 10,
+        defaultDecimalPoint: 1,
+        memory: [],
+        memorySize: 100000,
+        pushToMemory : function (tick) {
+            this.memory.unshift(tick);
+            // So we remove the last element from out memory if it exceed the memory size.
+            if (this.memory.length > this.memorySize) {
+                this.memory.pop();
+            }
+        },
+        buySignal: function(tick) {
+            this.buy = false;
+            this.pushToMemory(tick);
+            var sma = new Sma(this.shortPeriod,this.longPeriod, this.memory, this.defaultDecimalPoint);
+            sma.parse();
+            if (sma.goldenCrossHint) {
+                this.buy = true;
+            }
+
+            return this.buy;
+        },
+        sellSignal: function(tick) {
+            this.sell = false;
+            // If we bought, then don't immediately sell.
+            if (this.buy) {
+                return false;
+            }
+            var sma = new Sma(this.shortPeriod,this.longPeriod, this.memory, this.defaultDecimalPoint);
+            sma.parse();
+            if (sma.deathCrossHint) {
+                this.sell = true;
+            }
+
+            return this.sell;
+        },
+        inputs: function() {
+            return [
+            {name: 'short period', property: 'shortPeriod', value: this.shortPeriod},
+            {name: 'long period', property: 'longPeriod', value: this.longPeriod},
+            {name: 'decimal point', property: 'defaultDecimalPoint', value: this.defaultDecimalPoint}
+            ];
+        }
+    })
+})();
