49b33c42ce85e8c336623c14bb29157fa3fc67e2
Joseph Smith <joseph.smith@worldfirst.com>
2015-07-02 14:48:50 +0100

    Remove commented-out code

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-
 angular.module('myApp', [
   'ngRoute',
 
@@ -15,19 +14,6 @@ angular.module('myApp', [
   'highcharts-ng'
 ]).
 config(function ($routeProvider, $locationProvider) {
-  // $routeProvider.
-  //   when('/view1', {
-  //     templateUrl: 'partials/partial1',
-  //     controller: 'MyCtrl1'
-  //   }).
-  //   when('/view2', {
-  //     templateUrl: 'partials/partial2',
-  //     controller: 'MyCtrl2'
-  //   }).
-  //   otherwise({
-  //     redirectTo: '/view1'
-  //   });
-
   $locationProvider.html5Mode(true);
 });
 
